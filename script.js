5343bcf752de1d9fad4b6fec7ca724d614d50d23
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2024-06-17 19:06:57 +0530

    all fields validated

--- script.js
+++ script.js
@@ -18,3 +18,50 @@ function validateName(){
 
     return true;
 }
+
+
+function validatePhone(){
+    var phone = document.getElementById("contact-phone").value;
+    if(phone.length == 0){
+        phoneError.innerHTML = "Phone no. is required";
+        return false;
+    }
+    if(phone.length !== 10){
+        phoneError.innerHTML = "Enter valid phone no.";
+        return false;
+    }
+    if(!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = "Only digits please";
+        return false;
+    }
+    phoneError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+}
+
+
+function validateEmail(){
+    var email = document.getElementById("contact-email").value;
+    if(email.length == 0){
+        emailEerror.innerHTML = "Email is required";
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[.][a-z]{2,4}$/)){
+        emailEerror.innerHTML = "Email invalid";
+        return false;
+    }
+    emailEerror.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+}
+
+
+function validateMessage(){
+    var message = document.getElementById("contact-message").value;
+    var required = 30;
+    var left = required - message.length;
+    if(message.length < required){
+        messageError.innerHTML = left + " more characters required";
+        return false;
+    }
+    messageError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+}
